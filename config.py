cf36981309c484f6c4ac33f5bf7e68ff8583d4cc
mateoduke <mateoalexiduke@gmail.com>
2022-04-21 00:31:10 -0400

    GYM-1: Added scripts & testing directory; updated configs

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+from flask_sqlalchemy import SQLAlchemy
+
 class Config(object):
     """Base Config object for flask project
     Other configs will inherit from this config
@@ -7,8 +9,16 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+
 
 class DevelopmentConfig(Config):
     ENV = 'development'
     DEVELOPMENT = True
     SECRET_KEY = 'dev'
+
+
+class ProductionConfig(Config):
+    ENV = 'production'
+    DEVELOPMENT = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,17 +1,26 @@
+import click
 import os
+
 from flask import Flask
+
 
 def create_app(config = None):
     app = Flask(__name__, instance_relative_config=True)
     if config is None:
         app.config.from_object('config.DevelopmentConfig')
     else:
-        app.config.from_mapping(config)
+        app.config.from_object(config)
 
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    # cli commands
+    @app.cli.command('create-user')
+    @click.argument('name')
+    def create_user(name):
+        print(name)
 
     @app.route('/')
     def hello():
